dc000bacfc51b0c4f7fa227dc0eae4e0765831e8
SamPetherick1898 <fuentessamuelignacio@gmail.com>
2022-07-10 22:26:41 -0500

    actualizacion acerca de

--- proyecto2/app.js
+++ proyecto2/app.js
@@ -29,6 +29,9 @@ app.use("/", index);
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
+const branchRoutes = require("./routes/branch.routes")
+app.use("/", branchRoutes)
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
